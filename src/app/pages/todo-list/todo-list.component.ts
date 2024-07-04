5aadfa536b7b2995a0126dd0dabdb3566239b4c1
yamil.lanz <yamil.lanz@externos-cl.cencosud.com>
2024-07-04 15:58:36 -0400

    add todo list header email

--- src/app/pages/todo-list/todo-list.component.ts
+++ src/app/pages/todo-list/todo-list.component.ts
@@ -1,8 +1,9 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { HeaderComponent } from '../../components/header/header.component';
 import { TodoListActiveComponent } from '../../components/todo-list-active/todo-list-active.component';
 import { FormTodoComponent } from '../../components/form-todo/form-todo.component';
 import { TodoListHistoryComponent } from '../../components/todo-list-history/todo-list-history.component';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-todo-list',
@@ -16,4 +17,11 @@ import { TodoListHistoryComponent } from '../../components/todo-list-history/tod
   templateUrl: './todo-list.component.html',
   styleUrl: './todo-list.component.scss',
 })
-export class TodoListComponent {}
+export class TodoListComponent {
+  email: string | null = null;
+  route = inject(ActivatedRoute);
+
+  ngOnInit(): void {
+    this.email = this.route.snapshot.paramMap.get('email');
+  }
+}
